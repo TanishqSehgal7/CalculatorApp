1b2d250dcd19cbe38b71d096eb078d6913ec4df5
TanishqSehgal7 <tsgl7246@gmail.com>
2019-10-25 01:29:46 +0530

    App Update

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -4,123 +4,80 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.annotation.IntegerRes
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ResourceBundle.clearCache
 import kotlin.math.sqrt
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    override fun onClick(view: View) {
+class MainActivity : AppCompatActivity() {
 
-            when (view.id) {
-            R.id.btn1 -> {
-
-
-            }
-            R.id.plusBtn -> {
-                var value1 = number1.text.toString().toFloat()
-                    var value2 = number1.text.toString().toFloat()
-                    number1.setText("${value1 + value2}")
-            }
-            R.id.subtractBtn -> {
-                var value1 = Integer.parseInt(number1.text.toString())
-                var value2 = number1.text.toString().toFloat()
-                number1.setText("${value1 - value2}")
-            }
-            R.id.divideBtn -> {
-                var value1 = Integer.parseInt(number1.text.toString())
-                var value2 = number1.text.toString().toFloat()
-                number1.setText("${value1 / value2}")
-
-            }
-            R.id.multiplyBtn -> {
-                var value1 = Integer.parseInt(number1.text.toString())
-                var value2 = number1.text.toString().toFloat()
-                number1.setText("${value1 * value2}")
-            }
-            R.id.buttonroot ->{
-                var value1 = number1.text.toString().toFloat()
-                number1.setText("${sqrt(value1.toDouble())}")
-            }
-            R.id.btnPercent -> {
-                var value1 = Integer.parseInt(number1.text.toString())
-                number1.setText("${value1 % 100}")
-            }
-
-            R.id.btnPT->{
-                number1.append(".")
-            }
-            R.id.btnC ->{
-                number1.setText("")
-            }
-            R.id.btnReciprocal ->{
-                var value1 = Integer.parseInt(number1.text.toString())
-                number1.setText("${1/value1}")
-            }
-            R.id.btnSQ->{
-                var value1 = Integer.parseInt(number1.text.toString())
-                number1.setText("${value1*value1}")
-            }
-            R.id.btn0->{
-                number1.append(btn0.text)
-            }
-            R.id.btn1D->{
-                number1.append(btn1D.text)
-            }
-            R.id.btn2->{
-                number1.append(btn2.text)
-            }
-            R.id.btn3->{
-                number1.append(btn3.text)
-            }
-            R.id.btn4->{
-                number1.append(btn4.text)
-            }
-            R.id.btn5->{
-                number1.append(btn5.text)
-            }
-            R.id.btn6->{
-                number1.append( btn6.text)
-            }
-            R.id.btn7->{
-                number1.append(btn7.text)
-            }
-            R.id.btn8->{
-                number1.append(btn8.text)
-            }
-            R.id.btn9->{
-                number1.append(btn9.text)
-            }
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        btn1.setOnClickListener {
-//            Log.i("TEXT", "Display Text ${et1.text}")
-//
-//        }
-        btn1.setOnClickListener(this)
-        plusBtn.setOnClickListener(this)
-        subtractBtn.setOnClickListener(this)
-        divideBtn.setOnClickListener(this)
-        multiplyBtn.setOnClickListener(this)
-        buttonroot.setOnClickListener(this)
-        btn1.setOnClickListener(this)
-        btn9.setOnClickListener(this)
-        btn8.setOnClickListener(this)
-        btn7.setOnClickListener(this)
-        btn6.setOnClickListener(this)
-        btn5.setOnClickListener(this)
-        btn4.setOnClickListener(this)
-        btn3.setOnClickListener(this)
-        btn2.setOnClickListener(this)
-        btn1D.setOnClickListener(this)
-        btn0.setOnClickListener(this)
-        btnPT.setOnClickListener(this)
-        btnC.setOnClickListener(this)
-        btnPercent.setOnClickListener(this)
-        btnSQ.setOnClickListener(this)
-        btnReciprocal.setOnClickListener(this)
+        updateText("")
+
+    }
+    val operationList: MutableList<String> = arrayListOf()
+    val numCache: MutableList<String> = arrayListOf()
+
+    private fun getString(items: List<String>, connect: String=""):String {
+        if(items.isEmpty())
+            return ""
+        return items.reduce { acc, s -> acc+connect+s }
+    }
+
+    private fun updateText(mainUIString: String){
+        val calculationString=getString(operationList,"")
+        var calculationTxtView=findViewById<View>(R.id.tv2) as TextView
+        calculationTxtView.text=calculationString
+
+        val ans=findViewById<View>(R.id.tv1) as TextView
+        ans.text=mainUIString
+
+    }
+
+    fun pressnum (view: View){
+        val button = view as Button
+        val numString=button.text
+        numCache.add (numString.toString())
+        val text = getString(numCache)
+        updateText(text)
+
+    }
+
+    fun operatorPress(view: View){
+        val button= view as Button
+        if(numCache.isEmpty())
+            return
+        operationList.add(getString(numCache))
+        numCache.clear()
+        operationList.add(button.text.toString())
+        updateText(button.text.toString())
+
+    }
+
+    private fun clearText(view: View){
+        operationList.clear()
+        numCache.clear()
+    }
+
+    fun allClear (view: View) {
+        clearText(view)
+        updateText("")
+    }
+
+    fun equalSmash (view: View) {
+        operationList.add (getString(numCache))
+        numCache.clear()
+
+        val calculator = ExpressionCalc()
+        val answer = calculator.calculate(operationList)
+
+        updateText("=" + answer.toString())
+        clearCache()
+
     }
 }
 
